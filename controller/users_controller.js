764a8e897dbbf69a85ff04d8751acadafbf4f307
Preeti Sharma <rishishwarpreeti2810@gamil.com>
2026-01-20 11:14:33 +0530

    sign-up & sign-in form

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,53 @@
+const User = require('../models/user');
 module.exports.profile = function (req , res){
     return res.render('users_profile',{
         title : "profile"
     });
+}
+//render the signup page
+module.exports.signUp = function(req , res){
+    return res.render('user_sign_up',{
+        title : "codeial | Sign Up"
+    })
+}
+//render the signin page
+module.exports.signIn = function(req , res){
+    return res.render('user_sign_in',{
+        title : "codeial | Sign In"
+    })
+}
+//get the sign up data
+module.exports.create = function (req , res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('/sign-up');
+    }
+/*   User.findOne({email : req.body.email},function(err , user){
+        if(err){console.log('error in sugn up'); return}
+
+            if(!user){
+                User.create(req.body , function (err , user){
+                    if(err){console.log('error in creating sign up'); return}
+                        return res.redirect('/users/sign-in')
+                })
+            }else{
+                return res.redirect('/sign-up');
+            }*/
+            User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    return User.create(req.body)
+                    .then(() => {
+                        return res.redirect('/users/sign-in');
+                    });
+                } else {
+                    return res.redirect('/sign-up');
+                }
+            })
+            .catch(err => {
+                console.log("Error in sign up:", err);
+            });
+        }
+//get the sign in data
+module.exports.createSession = function (req , res){
+    //todo
 }
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 2006;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(expressLayouts);
 app.use('/', require('./routes'));
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
 //extract style & script from sub pages
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);

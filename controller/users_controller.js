3bc7478e8e401fef465798298550c37e51e638f4
Preeti Sharma <rishishwarpreeti2810@gamil.com>
2026-03-22 19:44:04 +0530

    testing the green flag

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -35,27 +35,27 @@ module.exports.profile = function (req , res){
 }
 //render the signup page
 module.exports.signUp = function(req , res){
-    if(req.isAuthenticated()){
+  /*  if(req.isAuthenticated()){
         return res.redirect('/users/profile');
-    }
+    }*/
     return res.render('user_sign_up',{
         title : "codeial | Sign Up"
     })
 }
 //render the signin page
 module.exports.signIn = function(req , res){
-    if(req.isAuthenticated()){
+    /*if(req.isAuthenticated()){
         return res.redirect('/users/profile');
-    }
+    }*/
     return res.render('user_sign_in',{
         title : "codeial | Sign In"
     })
 }
 //get the sign up data
 module.exports.create = function (req , res){
-    if(req.body.password != req.body.confirm_password){
+  /*  if(req.body.password != req.body.confirm_password){
         return res.redirect('/sign-up');
-    }
+    }*/
 /*   User.findOne({email : req.body.email},function(err , user){
         if(err){console.log('error in sugn up'); return}
 
